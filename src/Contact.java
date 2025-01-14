3c209a7a3090df429e05a2931efea82996785266
Aakriti0318 <136609419+Aakriti0318@users.noreply.github.com>
2025-01-14 21:19:21 +0530

    Update Contact.java

--- src/Contact.java
+++ src/Contact.java
@@ -8,13 +8,13 @@ class Contact {
             String sql = "INSERT INTO contacts (firstname,lastname, phone_number, email, address, city, state, zip) VALUES (?, ?, ?, ?, ?, ?, ?)";
             try (PreparedStatement statement = connection.prepareStatement(sql)) {
                 statement.setString(1, "joe");
-                statement.setString(1, "Davis");
-                statement.setString(2, "9145862394");
-                statement.setString(3, "joeDavis123@gmail.com");
-                statement.setString(4, "123B Washington");
-                statement.setString(5,"Virginia");
-                statement.setString(6, "Washington DC");
-                statement.setString(7, "99403");
+                statement.setString(2, "Davis");
+                statement.setString(3, "9145862394");
+                statement.setString(4, "joeDavis123@gmail.com");
+                statement.setString(5, "123B Washington");
+                statement.setString(6,"Virginia");
+                statement.setString(7, "Washington DC");
+                statement.setString(8, "99403");
                 statement.executeUpdate();
                 System.out.println("Contact added Successfully");
             }
